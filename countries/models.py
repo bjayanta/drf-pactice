1bbd0569148b876316f7b0bd884aacdd231b076b
Jayanta <bjayanta.me@gmail.com>
2022-11-02 00:04:25 +0600

    work with countries model

--- countries/models.py
+++ countries/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+class Countries(models.Model):
+    name = models.CharField(max_length=100)
--- countries/views.py
+++ countries/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+
+from .models
 
 # Create your views here.
+class ListView(APIView):
+    
+    def get(self, request):
+        countries = Co
+
